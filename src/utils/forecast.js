6226fe404c3506f359df748be2b17cac6c222234
Simonffy Ákos <simonffyakos@Simonffys-MacBook-Air.local>
2019-11-04 10:41:07 +0100

    add low and high temperature and fix bugs

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + Math.round(body.currently.temperature) + ' degrees out. Lowest temperature: ' + Math.round(body.daily.data[0].temperatureLow) + ' degrees. Highest temperature: ' + Math.round(body.daily.data[0].temperatureHigh) + ' degrees. There is a ' + body.currently.precipProbability * 100 + '% chance of rain.')
         }
     })
 }
